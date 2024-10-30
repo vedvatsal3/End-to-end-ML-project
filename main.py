0d5dab319ab134a80ec6a6aa33e65fd53245e26f
Your Name <vedvatsal3@gmail.com>
2024-10-30 11:18:28 +0100

    data validation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.mlProject import logger
 from src.mlProject.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlProject.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -11,3 +12,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e 
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<\n\nx========")
+except Exception as e:
+    logger.exception(e)
+    raise e 
+
